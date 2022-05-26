9f2d5ba291760802ca12fdce63664c94f19ddc45
najimpx <91430231+najimpx@users.noreply.github.com>
2022-05-26 11:16:34 -0300

    Add files via upload

---
+++ Cliente/Comando.java
@@ -0,0 +1,42 @@
+package Cliente;
+
+public class Comando {
+	
+	/*Faz o cliente se conectar a outro servidor e muda o reposit´orio corrente. Este comando
+	recebe o nome de um reposit´orio e obt´em do servi¸co de nomes uma referˆencia para esse
+	reposit´orio, que passa a ser o reposit´orio corrente.*/
+	public void bind() {
+		
+	}
+	/*Lista as pe¸cas do reposit´orio corrente*/
+	public void listp() {
+		
+	}
+	/*Busca uma pe¸ca por c´odigo. A busca ´e efetuada no reposit´orio corrente. Se encontrada,
+	a pe¸ca passa a ser a nova pe¸ca corrente.*/
+	public void getp() {
+		
+	}
+	/*Mostra atributos da pe¸ca corrente.*/
+	public void showp() {
+		
+	}
+	/* Esvazia a lista de sub-pe¸cas corrente*/
+	public void clearlist() {
+		
+	}
+	/*Adiciona `a lista de sub-pe¸cas corrente n unidades da pe¸ca corrente*/
+	public void addsubpart() {
+		
+	}
+	/*Adiciona uma pe¸ca ao reposit´orio corrente. A lista de sub-pe¸cas corrente ´e usada como
+lista de subcomponentes diretos da nova pe¸ca. (E s´o para isto que existe a lista de ´
+sub-pe¸cas corrente.)*/
+	public void addp() {
+		
+	}
+	/*Encerra a execu¸c˜ao do cliente*/
+	public void quit() {
+		
+	}
+}

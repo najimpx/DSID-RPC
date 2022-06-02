9d1473df78708d6f8308e11bbb6022db8e6843e4
najimpx <91430231+najimpx@users.noreply.github.com>
2022-06-02 11:08:43 -0300

    Delete Part.java

--- Part.java
+++
@@ -1,8 +0,0 @@
-import java.util.HashMap;	
-import java.util.Map;
-public interface Part {
-	public int codigo = 0;
-	public String nome = null;
-	public String 	descricao = null;
-	public Map<Part,Integer> subComp= new HashMap<Part,Integer>();
-}
